baf0dedd72f2ced4873865e4d05fd3d1a3f6bb09
B. O'Toole <botoole518@gmail.com>
2015-06-25 13:05:20 -0400

    setup + added header

--- web/app/themes/sage-master/templates/header.php
+++ web/app/themes/sage-master/templates/header.php
@@ -1,6 +1,7 @@
 <header class="banner" role="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+  <div class="container clearfix">
+    <a id="logo" class="brand" href="<?= esc_url(home_url('/')); ?>">
+    <span><?php bloginfo('name'); ?></span></a>
     <nav role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
